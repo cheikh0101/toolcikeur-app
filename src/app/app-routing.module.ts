7f86699038de5bf88529e3ce29095a3f6a63540b
mohamed <mohamedgithub69@gmail.com>
2022-04-09 03:15:31 +0000

    users frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,28 +8,37 @@ import { HomeComponent } from './components/home/home.component';
 import { PostDetailsComponent } from './components/sensibilisation/post-details/post-details.component';
 import { PostsListComponent } from './components/sensibilisation/posts-list/posts-list.component';
 import { InscriptionComponent } from './components/user/inscription/inscription.component';
+import { AsksListComponent } from './components/echange/asks-list/asks-list.component';
+import { AsksDetailsComponent } from './components/echange/asks-details/asks-details.component';
 
 const routes: Routes = [
   { 'path': '', component: HomeComponent },
-  { 'path': 'user', children: [
-    { 'path': 'connexion', component: ConnexionComponent },
-    { 'path': 'inscription', component: InscriptionComponent},
-    
-  ]
-},
-  { 'path': 'accompagnement', component: AccompagnementComponent },
   {
-      path: 'sensibilisation', children: [
-        { path: '', component: PostsListComponent },
-        { path: 'details', component: PostDetailsComponent },
-      ],
+    'path': 'echange', children: [
+      { 'path': 'ask-list', component: AsksListComponent },
+      { 'path': 'ask-details', component: AsksDetailsComponent }
+    ]
   },
   {
-      path: 'formation', children: [
-        { path: '', component: FormationListComponent },
-        { path: 'details', component: FormationDetailsComponent },
-      ],
-    },
+    'path': 'user', children: [
+      { 'path': 'connexion', component: ConnexionComponent },
+      { 'path': 'inscription', component: InscriptionComponent },
+
+    ]
+  },
+  { 'path': 'accompagnement', component: AccompagnementComponent },
+  {
+    path: 'sensibilisation', children: [
+      { path: '', component: PostsListComponent },
+      { path: 'details', component: PostDetailsComponent },
+    ],
+  },
+  {
+    path: 'formation', children: [
+      { path: '', component: FormationListComponent },
+      { path: 'details', component: FormationDetailsComponent },
+    ],
+  },
 
 
 ];
